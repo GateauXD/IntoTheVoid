751ff2153c0459a8ae7898ebd1cfccfb191f9757
Angelo Kyrilov <akyrilov@ucmerced.edu>
2018-05-05 10:34:29 -0700

    new template with added TexRect contructor for explosion

--- App.h
+++ App.h
@@ -2,26 +2,46 @@
 #define App_hpp
 
 #include "GlutApp.h"
-#include "Game.h"
+#include "TexRect.h"
+#include "AnimatedRect.h"
 
 class App: public GlutApp {
     // Maintain app state here
     float mx;
     float my;
-
-	Game *game;
 public:
     // Constructor, to initialize state
     App(const char* label, int x, int y, int w, int h);
-	~App() {
-		delete game;
-	}
     
     // These are the events we want to handle
     void draw();
     void keyPress(unsigned char key);
     void mouseDown(float x, float y);
     void mouseDrag(float x, float y);
+    
+    void specialKeyPress(int key);
+    
+    void specialKeyUp(int key);
+    
+    void idle();
+    
+    TexRect* ball;
+    
+    TexRect* platform;
+    
+    TexRect* background;
+    
+    AnimatedRect* gameOver;
+    
+    bool up;
+    bool down;
+    bool left;
+    bool right;
+    
+    bool moving;
+    bool game_over;
+    
+    
 };
 
 #endif
--- app_main.d
+++ app_main.d
@@ -1,2 +1,2 @@
-app_main.o: app_main.cpp App.h GlutApp.h Game.h Player.h Ship.h Object.h \
- Rect.h Shape.h TexRect.h RgbImage.h Enemy.h
+app_main.o: app_main.cpp App.h GlutApp.h TexRect.h SOIL/includes/SOIL.h \
+ AnimatedRect.h
